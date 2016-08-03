3c930ee4167dc58094eb22e441692b02f49ab693
Elizabeth <elizabethcphillips63@gmail.com>
2016-08-03 16:31:02 -0700

    Buttons wired up

--- app/src/main/java/com/garrettshorr/multitac/MainActivity.java
+++ app/src/main/java/com/garrettshorr/multitac/MainActivity.java
@@ -2,65 +2,69 @@ package com.garrettshorr.multitac;
 
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private final IntentFilter intentFilter = new IntentFilter();
-    private WifiP2pManager.Channel channel;
-    private WifiP2pManager manager;
-    private MultiTacReceiver receiver;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button topLeftButton, topCenterButton, topRightButton;
+    private Button centerLeftButton, centerCenterButton, centerRightButton;
+    private Button bottomLeftButton, bottomCenterButton, bottomRightButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //  Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        wireWidgets();
+        setUpOnClickButtons();
 
-        // Indicates a change in the list of available peers.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-
-        // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-
-        // Indicates this device's details have changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        //sets up the channel manager which connects your app to the P2P service
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
-
-        //this sets up the custom receiver class
-        receiver = new MultiTacReceiver(manager, channel, this);
-
-        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(MainActivity.this, "FOUND YOU!", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onFailure(int reasonCode) {
-                Toast.makeText(MainActivity.this, "WOMP WOMP WOMP :( " + reasonCode, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
-    /* register the broadcast receiver with the intent values to be matched */
     @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(receiver, intentFilter);
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.topLeftButton :
+                Toast.makeText(MainActivity.this,"button1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.topCenterButton :
+                Toast.makeText(MainActivity.this,"button2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.topRightButton :
+                Toast.makeText(MainActivity.this,"button3", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
-    /* unregister the broadcast receiver */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
+
+    public void wireWidgets(){
+        topLeftButton = (Button) findViewById(R.id.topLeftButton);
+        topCenterButton = (Button) findViewById(R.id.topCenterButton);
+        topRightButton = (Button) findViewById(R.id.topRightButton);
+        centerLeftButton = (Button) findViewById(R.id.centerLeftButton);
+        centerCenterButton = (Button) findViewById(R.id.centerCenterButton);
+        centerRightButton = (Button) findViewById(R.id.centerRightButton);
+        bottomLeftButton = (Button) findViewById(R.id.bottomLeftButton);
+        bottomCenterButton = (Button) findViewById(R.id.bottomCenterButton);
+        bottomRightButton = (Button) findViewById(R.id.bottomRightButton);
+    }
+
+    public void setUpOnClickButtons(){
+        topLeftButton.setOnClickListener(this);
+        topCenterButton.setOnClickListener(this);
+        topRightButton.setOnClickListener(this);
+        centerLeftButton.setOnClickListener(this);
+        centerCenterButton.setOnClickListener(this);
+        centerRightButton.setOnClickListener(this);
+        bottomLeftButton.setOnClickListener(this);
+        bottomCenterButton.setOnClickListener(this);
+        bottomRightButton.setOnClickListener(this);
     }
 
 
