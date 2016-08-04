9637fa0d1efbbb69c966ed20a48b9fcd776043fb
Elizabeth <elizabethcphillips63@gmail.com>
2016-08-03 20:18:40 -0700

    Buttons wired up

--- app/src/main/java/com/garrettshorr/multitac/MainActivity.java
+++ app/src/main/java/com/garrettshorr/multitac/MainActivity.java
@@ -8,22 +8,91 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.peak.salut.Callbacks.SalutCallback;
+import com.peak.salut.Callbacks.SalutDataCallback;
+import com.peak.salut.Callbacks.SalutDeviceCallback;
+import com.peak.salut.Salut;
+import com.peak.salut.SalutDataReceiver;
+import com.peak.salut.SalutDevice;
+import com.peak.salut.SalutServiceData;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SalutDataCallback {
+    private static final String TAG = MainActivity.class.getSimpleName() ;
     private Button topLeftButton, topCenterButton, topRightButton;
     private Button centerLeftButton, centerCenterButton, centerRightButton;
     private Button bottomLeftButton, bottomCenterButton, bottomRightButton;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SalutDataReceiver dataReceiver = new SalutDataReceiver(MainActivity.this, MainActivity.this);
+        SalutServiceData serviceData = new SalutServiceData("TicTacToe", 50489, "MultiTac");
+
+        Salut network = new Salut(dataReceiver, serviceData, new SalutCallback() {
+            @Override
+            public void call() {
+                Log.e(TAG, "Sorry, but this device does not support WiFi Direct.");
+            }
+        });
+
+        network.startNetworkService(new SalutDeviceCallback() {
+            @Override
+            public void call(SalutDevice device) {
+                Log.d(TAG, device.readableName + " has connected!");
+            }
+        });
+
+        network.discoverNetworkServices(new SalutDeviceCallback() {
+            @Override
+            public void call(SalutDevice device) {
+                Log.d(TAG, "A device has connected with the name " + device.deviceName);
+            }
+        }, false);
+
+        //OR
+
+        network.discoverNetworkServices(new SalutCallback() {
+            @Override
+            public void call() {
+                Log.d(TAG, "All I know is that a device has connected.");
+            }
+        }, true);
+        /*
+        network.discoverNetworkServicesWithTimeout(new SalutCallback() {
+            @Override
+            public void call() {
+                Log.d(TAG, "Look at all these devices! " + network.foundDevices.toString());
+            }
+        }, new SalutCallback() {
+            @Override
+            public void call() {
+                Log.d(TAG, "Bummer, we didn't find anyone. ");
+            }
+        }, 5000);
+
+        network.registerWithHost(possibleHost, new SalutCallback() {
+            @Override
+            public void call() {
+                Log.d(TAG, "We're now registered.");
+            }
+        }, new SalutCallback() {
+            @Override
+            public void call() {
+                Log.d(TAG, "We failed to register.");
+            }
+        });
+        */
         wireWidgets();
         setUpOnClickButtons();
+
 
     }
 
@@ -42,7 +111,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+    @Override
+    public void onDataReceived(Object o) {
 
+    }
     public void wireWidgets(){
         topLeftButton = (Button) findViewById(R.id.topLeftButton);
         topCenterButton = (Button) findViewById(R.id.topCenterButton);
@@ -66,6 +138,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bottomCenterButton.setOnClickListener(this);
         bottomRightButton.setOnClickListener(this);
     }
+
 
 
 }
